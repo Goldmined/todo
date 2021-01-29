c54189fab89a9e05653371ad1ab3a2fb35325aea
s37121 <goldmined666@gmail.com>
2021-01-29 16:42:44 +0300

    feff

--- array2.js
+++ array2.js
@@ -4,3 +4,10 @@ let arr = [9,8,7,5,5]
 //find 2
 //slice [4,2]
 //splice [2,2]
+
+let arr2 = arr.map(function(n,i){
+   return Math.trunc(n/2);
+})
+console.log(arr2);
+
+let arr3 = arr2.filter((n,i) => (n))
